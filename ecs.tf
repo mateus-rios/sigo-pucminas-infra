d0fb7fc633f3eeeb72a3a3234da74e8ba4cde48d
Mateus Rios <mateus.rios@interaconsultoria.com.br>
2021-03-08 12:40:18 -0300

    Ajustes

--- ecs.tf
+++ ecs.tf
@@ -2,11 +2,19 @@ resource "aws_ecs_cluster" "main" {
   name = "sigo-cluster"
 }
 
+data "template_file" "sigo_gn_service" {
+  template = "${file("task-definitions/sigo_gn.json")}"
+
+  vars = {
+   repository_url = "${aws_ecr_repository.gestao_normas.repository_url}"
+  }
+}
+
 resource "aws_ecs_task_definition" "sigo_gn_service" {
     family = "sigo_gn_service"
-    execution_role_arn = "arn:aws:iam::651358974252:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
+    execution_role_arn = "arn:aws:iam::651358974252:role/ecsTasks"
     network_mode   = "awsvpc"
-    container_definitions = file("task-definitions/sigo_gn.json")
+    container_definitions = "${data.template_file.sigo_gn_service.rendered}"
     requires_compatibilities = ["FARGATE"]
     cpu = 1024
     memory = 2048
@@ -22,8 +30,7 @@ resource "aws_ecs_service" "sigo_gn_service" {
 
   network_configuration {
     security_groups  = ["${aws_security_group.ecs_tasks.id}"]
-    subnets          = "${aws_subnet.private.*.id}"
-    assign_public_ip = true
+    subnets          = "${aws_subnet.private.*.id}"   
   }
 
   load_balancer {
